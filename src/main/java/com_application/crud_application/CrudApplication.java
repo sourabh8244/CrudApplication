4e261c592a3f88e02ab9277d330173f196c1de8d
sourabh8244 <sourabhbhattacharya8244@gmail.com>
2023-09-13 20:57:49 +0530

    initial commit

---
+++ src/main/java/com_application/crud_application/CrudApplication.java
@@ -0,0 +1,13 @@
+package com_application.crud_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudApplication.class, args);
+	}
+
+}
