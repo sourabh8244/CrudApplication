109e6158dc8313def67e721ea25138730b4304c3
sourabh8244 <sourabhbhattacharya8244@gmail.com>
2023-09-14 07:52:01 +0530

    rectified return logic of updateUser method in UserService

---
+++ src/main/java/service/UserService.java
@@ -0,0 +1,43 @@
+package service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import dao.UserRepository;
+import entity.User;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public User createUser(User user){
+        return userRepository.save(user);
+    }
+
+    public List<User> createUser(List<User> users){
+        return userRepository.saveAll(users);
+    }
+
+    public User getUserById(int id){
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public List<User> getUsers(){
+        return userRepository.findAll();
+    }
+
+    public User updatUser(User user){
+        Optional<User> optionalUser = userRepository.findById(user.getId());
+        if(optionalUser.isPresent()){
+            User oldUser = optionalUser.get();
+            oldUser.setName(user.getName());
+            oldUser.setAddress(user.getAddress());
+            return userRepository.save(oldUser);
+        }
+        else return null;
+    }
+}
